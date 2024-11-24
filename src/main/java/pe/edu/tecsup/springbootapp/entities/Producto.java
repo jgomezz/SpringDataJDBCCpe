8241e36960e12093b1981d954dc792429244b6cc
jgomezz <jgomezz@gmail.com>
2024-11-24 08:37:12 -0500

    update

--- src/main/java/pe/edu/tecsup/springbootapp/entities/Producto.java
+++ src/main/java/pe/edu/tecsup/springbootapp/entities/Producto.java
@@ -1,4 +1,26 @@
 package pe.edu.tecsup.springbootapp.entities;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+@Getter
+@Setter
 public class Producto {
+
+    private Long id;
+    private Long categorias_id;
+    private Categoria categoria;
+    private String nombre;
+    private String descripcion;
+    private Double precio;
+    private Integer stock;
+    private String imagen_nombre;
+    private String imagen_tipo;
+    private Long imagen_tamanio;
+    private Integer estado;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date creado;
 }
